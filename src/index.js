7666444fc8be3f302db8f6435e47ab0e9ca53e2e
Anna Perkowska <perkowska.a@gmail.com>
2018-05-13 23:26:27 +0200

    Add content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import DevTools from './DevTools';
+import { getCountries } from './actions/actions-countries';
+
 
 render(
-    <h1>Inicjalizacja projektu</h1>,
+	<Provider store={store}>
+    	<div>
+    		<h1>Inicjalizacja projektu</h1>
+    		<DevTools/>
+    	</div>
+    </Provider>,
     document.getElementById('root')
 );
+
+store.dispatch(getCountries());
